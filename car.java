48e900919b5c52700032e915b4e21f6513492305
alexanderwahlstrom <alewah09@hotmail.com>
2025-01-29 16:17:27 +0100

    u2 done

--- car.java
+++ car.java
@@ -1,17 +1,24 @@
 import java.awt.*;
 
-public class Car{
-    public int nrDoors;
-    public double enginePower;
-    public double currentSpeed;
-    public Color color;
-    public String modelName;
+public abstract class Car implements Movable{
+    protected int nrDoors;
+    protected double enginePower;
+    protected double currentSpeed;
+    protected Color color;
+    protected String modelName;
+    protected int dirIndex;
+    protected final char[] directions = {'N','E','S','W'};
+    protected double x;
+    protected double y;
 
     public Car(int drs, Color clr, double engPow, String modName){
         nrDoors = drs;
         color = clr;
         enginePower = engPow;
         modelName = modName;
+        dirIndex = 0;
+        x = 0;
+        y = 0;
         stopEngine();
     }
 
@@ -43,5 +50,46 @@ public class Car{
         currentSpeed = 0;
     }
 
+    @Override
+    public void turnLeft(){
+        dirIndex = (dirIndex + 3) % 4;
+    }
+
+    @Override
+    public void turnRight(){
+        dirIndex = (dirIndex + 1) % 4;
+    }
+
+    @Override
+    public void move(){
+        switch(directions[dirIndex]){
+            case 'N':
+                y += currentSpeed;
+                break;
+            case 'E':
+                x += currentSpeed;
+                break;
+            case 'S':
+                y -= currentSpeed;
+                break;
+            case 'W':
+                x -= currentSpeed;
+                break;
+        }
+    }
+
+    protected abstract double speedFactor();
+
+    protected abstract void incrementSpeed(double amount);
+
+    protected abstract void decrementSpeed(double amount);
+
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
     
 }
