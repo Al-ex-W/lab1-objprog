4241bf25d914cc067ad3f0fe15e91fa0192138d1
RadmehrDaniel <reza-radmehr@hotmail.com>
2025-02-06 12:35:55 +0100

    6 februari fram till 12.36 vid rum sb411

--- Scania.java
+++ Scania.java
@@ -11,22 +11,49 @@ public class Scania extends Car{
         trunkAngle = 0;
     }
 
+    public double getTrunkAngle() {
+        return trunkAngle;
+    }
+
     public void setTrunkAngle(double amount)    {
-        double newAngle = (trunkAngle + amount);
+        double newAngle = amount;
 
         if(newAngle < 0 || newAngle > 70)   {
-            //exception
+            System.out.println("Angle must be between 0 and 70.");
+            //ksk exception?
         } else if(getCurrentSpeed()!=0) {
-            //exception
+            System.out.println("Angle can only be changed at standstill.");
+            //ksk exception?
         } else {
             trunkAngle = newAngle;
         }
     }
 
+    //vi behöver 2 til funktioner: lowerTrunkAngle och 
+
+    public double lowerTrunkAngle(double amount) {
+        double newAngle = getTrunkAngle() - amount;
+        return(newAngle);
+    }
+
+    public double RaiseTrunkAngle(double amount) {
+        double newAngle = getTrunkAngle() + amount;
+        return(newAngle);
+    }
+    
+    @Override
+    public void gas(double amount) {
+        
+        if (trunkAngle != 0) {
+            //om trunk angel är höjt
+            System.out.println("cannot gas while trunk is raised.");
+            //exception??
+        } else {
+            super.gas(amount);
+        }
+    }
     // använd super.gas()
 
-    if trunkAngle < 0 {
-        
-    }
+   
 
 }
